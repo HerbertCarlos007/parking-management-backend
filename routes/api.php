84a32bacddb621a06ec9aa849df8956405076f30
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2026-01-06 21:58:24 -0300

    feat: colocando as rotas no auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -10,23 +10,37 @@ use Illuminate\Support\Facades\Route;
 Route::post('/users', [UserController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
 Route::get('/users', [UserController::class, 'index']);
-Route::put('/users/{user}', [UserController::class, 'update']);
-Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::post('/clients', [ClientController::class, 'store']);
-Route::get('/clients', [ClientController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
 
-Route::post('/parking-spots', [ParkingSpotController::class, 'store']);
-Route::get('/parking-spots', [ParkingSpotController::class, 'index']);
-Route::get('/parking-spots-available', [ParkingSpotController::class, 'getParkingSpotsAvailables']);
-Route::get('/parking-spots-status', [ParkingSpotController::class, 'getParkingSpotsStatus']);
-Route::get('/spots-stats', [ParkingSpotController::class, 'getSpotsStats']);
+    Route::post('/clients', [ClientController::class, 'store']);
+    Route::get('/clients', [ClientController::class, 'index']);
 
-Route::post('/parking-entries', [ParkingEntriesController::class, 'store']);
-Route::get('/parking-entries/{status}', [ParkingEntriesController::class, 'index']);
-Route::put('/parking-entries/{parkingEntry}', [ParkingEntriesController::class, 'update']);
+    Route::post('/parking-spots', [ParkingSpotController::class, 'store']);
+    Route::get('/parking-spots', [ParkingSpotController::class, 'index']);
+    Route::get('/parking-spots-available', [ParkingSpotController::class, 'getParkingSpotsAvailables']);
+    Route::get('/parking-spots-status', [ParkingSpotController::class, 'getParkingSpotsStatus']);
+    Route::get('/spots-stats', [ParkingSpotController::class, 'getSpotsStats']);
+
+    Route::post('/parking-entries', [ParkingEntriesController::class, 'store']);
+    Route::get('/parking-entries/{status}', [ParkingEntriesController::class, 'index']);
+    Route::get('all-parking_entries', [ParkingEntriesController::class, 'getAllParkingEntries']);
+    Route::put('/parking-entries/{parkingEntry}', [ParkingEntriesController::class, 'update']);
+});
+
+
+
+
+
+
+
+
+
